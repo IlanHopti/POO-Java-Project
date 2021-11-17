feabbb1cd8b0294d479c4faab057b772f287d760
Theo Landemaine <theo.landemaine@hotmail.fr>
2021-11-17 11:00:57 +0100

    Change MainCaracter

--- src/com/company/MainCharacter.java
+++ src/com/company/MainCharacter.java
@@ -1,9 +1,30 @@
 package com.company;
 
-import org.w3c.dom.ls.LSOutput;
+import java.util.Scanner;
 
-public class MainCharacter {
-    public static void MainCharacter (String[] args) {
-        System.out.println("hello");
+class Character {
+
+    void caracteristics() {
+
+        System.out.println("Entrez votre nom");
+        Scanner nom = new Scanner(System.in);
+        String name = nom.nextLine();
+
+        System.out.println("Entrez vos dégats");
+        Scanner degats = new Scanner(System.in);
+        String damage = degats.nextLine();
+
+        System.out.println("Entrez vos points de vies");
+        Scanner vie = new Scanner(System.in);
+        String life = vie.nextLine();
+
+        System.out.println("Entrez vos points d'initiatives");
+        Scanner initiaves = new Scanner(System.in);
+        String initiative = initiaves.nextLine();
+
+        System.out.println("Your name's character is " + name + ", inflict " + damage
+                + " damages, has " + life + " lifes points and has " + initiative + " initiatives points");
     }
+
 }
+
