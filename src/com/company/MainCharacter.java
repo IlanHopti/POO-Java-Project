691308006ed1a4337703dc4a75ad638cb648a37b
VaheKri <91196072+VaheKri@users.noreply.github.com>
2021-11-17 10:49:02 +0100

    Create MainCharacter.java

--- src/com/company/MainCharacter.java
+++ src/com/company/MainCharacter.java
@@ -1,4 +1,9 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MainCharacter {
+    public static void MainCharacter (String[] args) {
+        System.out.println("hello");
+    }
 }
