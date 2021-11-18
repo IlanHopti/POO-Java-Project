e50256474a32150bb2021dcbb5dcbbba71e72d20
Theo Landemaine <theo.landemaine@hotmail.fr>
2021-11-18 15:20:09 +0100

    Add wizard capacity + warrior attack

--- src/com/company/Fight.java
+++ src/com/company/Fight.java
@@ -5,15 +5,15 @@ public class Fight {
     void damage(Character player, Character player2) {
 
         if (player.initiative > player2.initiative) {
-            player2.life = player2.life - player.damage;
-            player.life = player.life - player2.damage;
+            player2.life = player2.life - player.damage + player2.shield;
+            player.life = player.life - player2.damage + player.shield;
 
             System.out.println(player2.name + " has " + player2.life + " pv and received " + player.damage + " damage");
             System.out.println(player.name + " has " + player.life + " pv and received " + player2.damage + " damage");
 
         } else if (player2.initiative > player.initiative) {
-            player.life = player.life - player2.damage;
-            player2.life = player2.life - player.damage;
+            player.life = player.life - player2.damage + player.shield;
+            player2.life = player2.life - player.damage + player2.shield;
 
             System.out.println(player.name + " has " + player.life + " pv and received " + player.damage + " damage");
             System.out.println(player2.name + " has " + player2.life + " pv and received " + player2.damage + " damage");
