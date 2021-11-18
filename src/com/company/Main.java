99e51d68932415806042d13407e1c8bc93b732ab
Theo Landemaine <theo.landemaine@hotmail.fr>
2021-11-18 11:43:16 +0100

    Delete choice mode + add type

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -37,127 +37,91 @@ class Menu {
         }else if(number == 2) {
             System.out.println("The game continue");
 
+            Fight test = new Fight();
+
+            Character player = new Character();
+
             System.out.println(" ");
-            System.out.println("Chose your mode : ");
-            System.out.println("1. Player VS AI");
-            System.out.println("2. Player1 VS Player2");
-            System.out.println(" ");
-            System.out.println("Enter the correct number : ");
+            System.out.println("Chose your character : ");
+            System.out.println("1 : Warrior");
+            System.out.println("2 : Wizard");
+            System.out.println("3 : Thief");
 
-            Scanner condition2 = new Scanner(System.in);
-            int number2 = condition2.nextInt();
+            Scanner choseCharacter = new Scanner(System.in);
+            int answer = choseCharacter.nextInt();
 
-            if (number2 == 1) {
-                Warrior player = new Warrior();
-                System.out.println(" ");
-                System.out.println("Player 1 :");
-                player.caracteristics();
-                stock.add(player.name);
-                stock.add(player.damage);
-                stock.add(player.life);
-                stock.add(player.initiative);
-                player.tellMe();
-
-                DefaultCharacter playerDefault = new DefaultCharacter();
-                stock.add(playerDefault.nameDefault);
-                stock.add(playerDefault.damageDefault);
-                stock.add(playerDefault.lifeDefault);
-                stock.add(playerDefault.initiativeDefault);
-
-                System.out.println("Player 1 : "+ player);
-                System.out.println("Player 2 :" + playerDefault);
-
-            }else if (number2 == 2) {
-                Fight test = new Fight();
-
-                Character player = new Character();
-
-                System.out.println("Chose your character");
-                System.out.println("1 : Warrior");
-                System.out.println("2 : Wizard");
-                System.out.println("3 : Thief");
-
-                Scanner choseCharacter = new Scanner(System.in);
-                int answer = choseCharacter.nextInt();
-
-                if (answer == 1) {
-                    player = new Warrior();
-                }else if (answer == 2) {
-                    player = new Wizard();
-                }else if (answer == 3) {
-                    player = new Thief();
-                }
-
-                System.out.println(" ");
-                System.out.println("Player 1 :");
-                player.caracteristics();
-                stock.add(player.name);
-                stock.add(player.damage);
-                stock.add(player.life);
-                stock.add(player.initiative);
-                player.tellMe();
-
-
-                Character player2 = new Character();
-
-                System.out.println("Chose your character");
-                System.out.println("1 : Warrior");
-                System.out.println("2 : Wizard");
-                System.out.println("3 : Thief");
-
-                Scanner choseCharacter2 = new Scanner(System.in);
-                int answer2 = choseCharacter2.nextInt();
-
-                if (answer2 == 1) {
-                    player2 = new Warrior();
-                }else if (answer2 == 2) {
-                    player2 = new Wizard();
-                }else if (answer2 == 3) {
-                    player2 = new Thief();
-                }
-                System.out.println(" ");
-                System.out.println("Player 2 :");
-                player2.caracteristics();
-                stock.add(player2.name);
-                stock.add(player2.damage);
-                stock.add(player2.life);
-                stock.add(player2.initiative);
-                player2.tellMe();
-
-                System.out.println("Player 1 : " + player);
-                System.out.println("Player 2 : " + player2);
-
-                System.out.println(" ");
-                System.out.println("Begin of the Fight");
-
-                boolean turn = true;
-                int counter = 1;
-
-                while (turn) {
-                    if (player.life <= 0) {
-                        turn = false;
-                        System.out.println(" ");
-                        System.out.println("The winner is " + player2.name);
-                    } else if (player2.life <= 0) {
-                        turn = false;
-                        System.out.println(" ");
-                        System.out.println("The winner is " + player.name);
-                    } else {
-                        System.out.println(" ");
-                        System.out.println("Turn n°" + counter + " :");
-                        test.damage(player, player2);
-                    }
-                    counter++;
-                }
-
-                System.out.println(" ");
-                System.out.println("End of the Fight");
-
-            }else {
-                System.out.println("This mode doesn't exist");
-                tellMe();
-                condition();
+            if (answer == 1) {
+                player = new Warrior();
+            }else if (answer == 2) {
+                player = new Wizard();
+            }else if (answer == 3) {
+                player = new Thief();
             }
+
+            System.out.println(" ");
+            System.out.println("Player 1 :");
+            player.caracteristics();
+            stock.add(player.name);
+            stock.add(player.damage);
+            stock.add(player.life);
+            stock.add(player.initiative);
+            stock.add(player.type);
+
+            Character player2 = new Character();
+
+            System.out.println(" ");
+            System.out.println("Chose your character : ");
+            System.out.println("1 : Warrior");
+            System.out.println("2 : Wizard");
+            System.out.println("3 : Thief");
+
+            Scanner choseCharacter2 = new Scanner(System.in);
+            int answer2 = choseCharacter2.nextInt();
+
+            if (answer2 == 1) {
+                player2 = new Warrior();
+            }else if (answer2 == 2) {
+                player2 = new Wizard();
+            }else if (answer2 == 3) {
+                player2 = new Thief();
+            }
+            System.out.println(" ");
+            System.out.println("Player 2 :");
+            player2.caracteristics();
+            stock.add(player2.name);
+            stock.add(player2.damage);
+            stock.add(player2.life);
+            stock.add(player2.initiative);
+            stock.add(player2.type);
+
+            System.out.println("Player 1 : " + player);
+            System.out.println("Player 2 : " + player2);
+
+            System.out.println(" ");
+            System.out.println("Begin of the Fight");
+
+            boolean turn = true;
+            int counter = 1;
+
+            while (turn) {
+                if (player.life <= 0) {
+                    turn = false;
+                    System.out.println(" ");
+                    System.out.println("The winner is " + player2.name + "who is a ");
+                } else if (player2.life <= 0) {
+                    turn = false;
+                    System.out.println(" ");
+                    System.out.println("The winner is " + player.name + ", who is a ");
+                } else {
+                    System.out.println(" ");
+                    System.out.println("Turn n°" + counter + " :");
+                    test.damage(player, player2);
+                }
+                counter++;
+            }
+
+            System.out.println(" ");
+            System.out.println("End of the Fight");
 
         }else {
             System.out.println("This feature doesn't exist");
--- src/com/company/MainCharacter.java
+++ src/com/company/MainCharacter.java
@@ -1,21 +1,22 @@
 package com.company;
 
+import java.lang.constant.Constable;
 import java.util.Scanner;
 
 class Character {
 
+    String type;
     String name;
     int damage;
     int life;
     int initiative;
-    int id;
 
     public Character() {
+        this.type = type;
         this.name = name;
         this.damage = damage;
         this.life = life;
         this.initiative = initiative;
-        this.id = id;
     }
 
     void caracteristics() {
@@ -43,9 +44,80 @@ class Character {
 
     public String toString() {
         return "You are " + name + ", he inflict " + damage
-                + " damage's points, he has " + life + " life's points and he has " + initiative + " initiative's points";
+                + " damage's points, he has " + life + " life's points, he has " + initiative +
+                " initiative's points";
     }
 }
+
+class Warrior extends Character{
+
+    String type = "Warrior";
+
+    public Warrior() {
+        this.type = type;
+    }
+
+    void tellMe() {
+        System.out.println("possède un bouclier qui réduit les dégâts infligés d'une valeur constante");
+    }
+
+    public String toString() {
+        return "You are " + name + ", he inflict " + damage
+                + " damage's points, he has " + life + " life's points, he has " + initiative +
+                " initiative's points and he is a " + type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
+
+class Wizard extends Character{
+
+    String type = "Wizard";
+
+    public Wizard() {
+        this.type = type;
+    }
+
+    void tellMe() {
+        System.out.println("inflige des dégâts magiques se rajoutant aux dégâts de base, " +
+                "mais ces dégâts magiques réduisent de moitié après chaque attaque");
+    }
+    public String toString() {
+        return "You are " + name + ", he inflict " + damage
+                + " damage's points, he has " + life + " life's points, he has " + initiative +
+                " initiative's points and he is a " + type;
+    }
+
+    public String toString(String type) {
+        return type;
+    }
+}
+
+class Thief extends Character{
+
+    String type = "Thief";
+
+    public Thief() {
+        this.type = type;
+    }
+
+    void tellMe() {
+        System.out.println("possède une probabilité d'esquiver totalement une attaque, " +
+                "et possède également une probabilité d'infliger un coup critique qui doublera les dégâts de base");
+    }
+    public String toString() {
+        return "You are " + name + ", he inflict " + damage
+                + " damage's points, he has " + life + " life's points, he has " + initiative +
+                " initiative's points and he is a " + type;
+    }
+
+    public String toString(String type) {
+        return type;
+    }
+}
+
 
 class DefaultCharacter extends Character{
 
@@ -63,29 +135,7 @@ class DefaultCharacter extends Character{
 
     public String toString() {
         return "The AI player is " + nameDefault + ", he inflict " + damageDefault
-                + " damage's points, he has " + lifeDefault + " life's points and he has " + initiativeDefault + " initiative's points";
-    }
-}
-
-class Warrior extends Character{
-
-    void tellMe() {
-        System.out.println("possède un bouclier qui réduit les dégâts infligés d'une valeur constante");
-    }
-}
-
-class Wizard extends Character{
-
-    void tellMe() {
-        System.out.println("inflige des dégâts magiques se rajoutant aux dégâts de base, " +
-                "mais ces dégâts magiques réduisent de moitié après chaque attaque");
-    }
-}
-
-class Thief extends Character{
-
-    void tellMe() {
-        System.out.println("possède une probabilité d'esquiver totalement une attaque, " +
-                "et possède également une probabilité d'infliger un coup critique qui doublera les dégâts de base");
+                + " damage's points, he has " + lifeDefault + " life's points, he has " + initiativeDefault +
+                " initiative's points";
     }
 }
