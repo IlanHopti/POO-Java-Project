8cab7e23954ffdc086e23efbc1965fa5f23e1f50
Theo Landemaine <theo.landemaine@hotmail.fr>
2021-11-18 09:40:20 +0100

    Add class

--- src/com/company/Fight.java
+++ src/com/company/Fight.java
@@ -8,15 +8,15 @@ public class Fight {
             player2.life = player2.life - player.damage;
             player.life = player.life - player2.damage;
 
-            System.out.println(player2.name + " has " + player2.life + " pv and received" + player.damage + " damage");
-            System.out.println(player.name + " has " + player.life + " pv and received" + player2.damage + " damage");
+            System.out.println(player2.name + " has " + player2.life + " pv and received " + player.damage + " damage");
+            System.out.println(player.name + " has " + player.life + " pv and received " + player2.damage + " damage");
 
         } else if (player2.initiative > player.initiative) {
             player.life = player.life - player2.damage;
             player2.life = player2.life - player.damage;
 
-            System.out.println(player.name + " has " + player.life + " pv and received" + player.damage + " damage");
-            System.out.println(player2.name + " has " + player2.life + " pv and received" + player2.damage + " damage");
+            System.out.println(player.name + " has " + player.life + " pv and received " + player.damage + " damage");
+            System.out.println(player2.name + " has " + player2.life + " pv and received " + player2.damage + " damage");
 
         }
     }
